d257a0dd6d73b7d8f04d291dd1ec8e47cfacbc21
Евгений <evgenij.romanko@neoseo.com.ua>
2020-09-10 11:45:27 +0300

    edin async function

--- contacts.js
+++ contacts.js
@@ -2,19 +2,19 @@ const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.resolve(__dirname, "bd", "contacts.json");
 
-const getContact = async () => {
+async function getContact() {
   const contactsData = await fs.readFile(contactsPath, {
     encoding: "utf-8",
   });
   return JSON.parse(contactsData);
-};
+}
 
-const listContacts = async () => {
+async function listContacts() {
   const list = await getContact();
   console.table(list);
-};
+}
 
-const addContact = async (name, email, phone) => {
+async function addContact(name, email, phone) {
   let contactsData = await getContact();
   const id = contactsData.length ? [...contactsData].pop().id + 1 : 1;
   const newContacts = { id, name, email, phone };
@@ -24,21 +24,21 @@ const addContact = async (name, email, phone) => {
   await fs.writeFile(contactsPath, contactsDataJson);
   contactsData = await getContact();
   console.table(contactsData);
-};
+}
 
-getContactById = async (id) => {
+async function getContactById(id) {
   const contactsData = await getContact();
   findContact = contactsData.find((contact) => contact.id === id);
   console.table(findContact);
-};
+}
 
-removeContact = async (id) => {
+async function removeContact(id) {
   let contactsData = await getContact();
   const result = contactsData.filter((contact) => contact.id !== id);
   await fs.writeFile(contactsPath, JSON.stringify(result));
   contactsData = await getContact();
   console.table(contactsData);
-};
+}
 
 module.exports = {
   listContacts,

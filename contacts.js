1df50bb715bef7fb1ff51fc653a2f27b6d3adc4a
Евгений <evgenij.romanko@neoseo.com.ua>
2020-09-17 22:10:12 +0300

    cleaner code

--- contacts.js
+++ contacts.js
@@ -31,7 +31,7 @@ async function getContactById(id) {
 
 async function removeContact(id) {
   let contactsData = await listContacts();
-  console.log(contactsData);
+  // console.log(contactsData);
   const result = contactsData.filter(contact => contact.id !== id);
   await fs.writeFile(contactsPath, JSON.stringify(result));
   contactsData = await listContacts();
@@ -65,15 +65,3 @@ module.exports = {
   addContact,
   getUpdateContact,
 };
-
-// async function updateContact(id, body) {
-//   const contacts = await listContacts();
-//   const indexById = await contacts.findIndex(contact => contact.id === id);
-//   contacts[indexById] = {
-//     ...contacts[indexById],
-//     ...body,
-//   };
-//   const contactAsJSON = JSON.stringify(contacts);
-//   await fs.writeFile(contactsPath, contactAsJSON);
-//   return contacts[indexById];
-// }
